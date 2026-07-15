9cee7d85121a6b828220193a9b724fd6d821d2ac
Valera <valeratrades@gmail.com>
2026-07-15 15:25:19 +0000

    -- New Project Snippet --

---
+++ flake.nix
@@ -0,0 +1,95 @@
+{
+  nixConfig = {
+    extra-substituters = [ "https://valeratrades.cachix.org" ];
+    extra-trusted-public-keys = [ "valeratrades.cachix.org-1:gXVwhzO5YB+BaiEJYT48qZgzdaErGQew6xtZcz4Fo1Q=" ];
+  };
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    flake-utils.url = "github:numtide/flake-utils";
+    pre-commit-hooks.url = "github:cachix/git-hooks.nix";
+    v_flakes.url = "github:valeratrades/v_flakes?ref=v1.6";
+  };
+  outputs = { self, nixpkgs, rust-overlay, flake-utils, pre-commit-hooks, v_flakes }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+          allowUnfree = true;
+        };
+        #NB: can't load rust-bin from nightly.latest, as there are week guarantees of which components will be available on each day.
+        rust = pkgs.rust-bin.selectLatestNightlyWith (toolchain: toolchain.default.override {
+          extensions = [ "rust-src" "rust-analyzer" "rust-docs" "rustc-codegen-cranelift-preview" ];
+        });
+        pre-commit-check = pre-commit-hooks.lib.${system}.run (v_flakes.files.preCommit { inherit pkgs; });
+        manifest = (pkgs.lib.importTOML ./Cargo.toml).package;
+        pname = manifest.name;
+        stdenv = pkgs.stdenvAdapters.useMoldLinker pkgs.stdenv;
+
+        rs = v_flakes.rs { inherit pkgs rust; };
+        github = v_flakes.github {
+          inherit pkgs pname rs;
+          enable = true;
+          lastSupportedVersion = "nightly-2026-07-14";
+          jobs.default = true;
+        };
+        readme = v_flakes.readme-fw {
+          inherit pkgs pname;
+          defaults = true;
+          lastSupportedVersion = "nightly-1.92";
+          rootDir = ./.;
+          badges = [ "msrv" "crates_io" "docs_rs" "loc" "ci" ];
+        };
+        combined = v_flakes.utils.combine { inherit rust; modules = [ rs github readme ]; };
+      in
+      {
+        packages =
+          let
+            rustc = rust;
+            cargo = rust;
+            rustPlatform = pkgs.makeRustPlatform {
+              inherit rustc cargo stdenv;
+            };
+          in
+          {
+            default = rustPlatform.buildRustPackage {
+              inherit pname;
+              version = manifest.version;
+
+              buildInputs = with pkgs; [
+                openssl.dev
+              ];
+              nativeBuildInputs = with pkgs; [ pkg-config ];
+
+              cargoLock.lockFile = ./Cargo.lock;
+              src = pkgs.lib.cleanSource ./.;
+            };
+          };
+
+        devShells.default =
+          with pkgs;
+          mkShell {
+            inherit stdenv;
+            shellHook =
+              pre-commit-check.shellHook
+              + combined.shellHook
+              + ''
+                cp -f ${(v_flakes.files.treefmt) { inherit pkgs; }} ./.treefmt.toml
+              '';
+
+            packages = [
+              mold
+              openssl
+              pkg-config
+              rust
+            ] ++ pre-commit-check.enabledPackages ++ combined.enabledPackages;
+
+						env.RUST_BACKTRACE = 1;
+						env.RUST_LIB_BACKTRACE = 0;
+          };
+      }
+    );
+}
